9c764cbb8660bb13d796787e2ea298b7f3e0b832
Aaron <aaronwan808@gmail.com>
2024-06-26 01:03:34 +0800

    added items counter to the top of the grid

--- src/components/ShoppingPage.jsx
+++ src/components/ShoppingPage.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import Card from './ShoppingCard';
 import Sidebar from './Sidebar';
+import { Typography } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 
 function ShoppingPage({ itemsarr }) {
   if (!itemsarr || itemsarr.length === 0) {
-    return <div>No items found.</div>;
+    return <Typography variant='h2' fontWeight={"bold"}>No Items Found</Typography>;
   }
 
   return (
-    <div className='flex justify-center items-center my-4 flex-row'>
-    <Sidebar/>
+    <div className='flex justify-center items-center my-4 flex-col'>
+    {/* <Sidebar/> */}
+    <Typography variant='h6' fontWeight={"bold"}>{`Items (${itemsarr.length})`}</Typography>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-4">
         {itemsarr.map((item, index) => (
           <Card
