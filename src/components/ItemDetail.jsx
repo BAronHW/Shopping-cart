dfad390b3acf7edb47fe07ff92dff0f15b9a0436
Aaron <aaronwan808@gmail.com>
2024-06-26 11:59:09 +0800

    making changes to itemdesc

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { CircularProgress, Typography } from '@mui/material';
+import { CircularProgress, Container, Typography, Button, IconButton } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
@@ -53,13 +53,19 @@ function ItemDetail() {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center min-h-screen">
       <img className="object-fill max-w-sm" src={`${item.image}`} alt={item.title} />
-      <Link to="/shopping">
-        <ArrowBackIcon />
-      </Link>
-      <div>
-        <Typography>{item.title}</Typography>
+      <div className='flex flex-col items-center justify-center gap-4 '>
+        <Container maxWidth="sm" >
+          <Typography variant='h4'>{item.title}</Typography>
+          <Typography variant='h7'>{item.description}</Typography>  
+          <Typography variant='h8'>{`Rating: ${item.rating.rate}`}</Typography>
+          <Typography variant='h8'>{`Rating: ${item.rating.count}`}</Typography>     
+          <Typography variant='h6'>{item.price}</Typography>               
+        </Container>
+        <IconButton component={Link} to="/shopping" className="fixed top-4 left-4">
+        <ArrowBackIcon fontSize="large" />
+      </IconButton>
       </div>
     </div>
   );
